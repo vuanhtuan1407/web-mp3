f414e51cedf3486b1a3ac979dd3c0094dca20abd
vuanhtuan1407 <vuanhtuan1407@gmail.com>
2022-12-19 20:10:18 +0700

    save

--- src/MusicList/List.js
+++ src/MusicList/List.js
@@ -6,7 +6,7 @@ export const list = [
     image:
       "https://cdn-japantimes.com/wp-content/uploads/2021/08/np_file_105988-870x489.jpeg",
     url: "https://drive.google.com/uc?export=view&id=1AgGUBxx1USLSU8o7JCGQheYGoHwFD6fm",
-    type: "EDM"
+    type: ["EDM"]
   },
   {
     id: 1,
@@ -14,7 +14,7 @@ export const list = [
     artist: "Blue Swede",
     image: "https://i.ytimg.com/vi/NrI-UBIB8Jk/mqdefault.jpg",
     url: "https://drive.google.com/uc?export=view&id=1H7MiuBR5ofRbdg7Mt2G3HrLrjEht_ehu",
-    type: "Pop Rock"
+    type: ["Pop Rock", "R&B/Soul"]
   },
   {
     id: 2,
@@ -22,7 +22,7 @@ export const list = [
     artist: "Lost Frequencies",
     image: "https://melodicpop.com/build/images/artists/lost-frequencies.png",
     url: "https://drive.google.com/uc?export=view&id=1WUy8BlH4gw8kQ72knJdzwtBSKKpLumOz",
-    type: "Deep House"
+    type: ["Deep House", "R&B/Soul", "Ukrainian Pop"]
   },
   {
     id: 3,
@@ -31,7 +31,7 @@ export const list = [
     image:
       "https://i1.sndcdn.com/artworks-s3zOCWcV8XQVtQcv-0emq8A-t500x500.jpg",
     url: "https://drive.google.com/uc?export=view&id=18Th6PJCf-0JrkSdIYQ5_epeLSxgNGqRv",
-    type: "Pop Rock"
+    type: ["Pop Rock", "Arena pop"]
   },
   {
     id: 4,
@@ -39,7 +39,7 @@ export const list = [
     artist: "Kungs vs Cookin’ on 3 Burner",
     image: "https://i1.sndcdn.com/artworks-000146780072-z8inm0-t500x500.jpg",
     url: "https://drive.google.com/uc?export=view&id=1rA7nX4hJcqi-PpBQAemkVZnLZxk3x0AY",
-    type: "Dance/Electronic"
+    type: ["Dance/Electronic"]
   },
   {
     id: 5,
@@ -47,7 +47,7 @@ export const list = [
     artist: "OneRepublic",
     image: "https://i1.sndcdn.com/artworks-000040814493-eu3kr3-t500x500.jpg",
     url: "https://drive.google.com/uc?export=view&id=1T_Ro100XjNMnnIOcywTX5KnLIYXRFADR",
-    type: "Pop Rock"
+    type: ["Pop Rock", "Folk-pop"]
   },
   {
     id: 6,
@@ -56,7 +56,7 @@ export const list = [
     image:
       "https://i1.sndcdn.com/artworks-YEwXu5sQShwND3Rc-r1FSRw-t500x500.jpg",
     url: "https://drive.google.com/uc?export=view&id=1CrCnTFEk9ATSQ-dAKYWMzu4rzz60eC1F",
-    type: "Disco"
+    type: ["Pop", "Disco", "Funk"]
   },
   {
     id: 7,
@@ -65,6 +65,6 @@ export const list = [
     image:
       "https://upload.wikimedia.org/wikipedia/vi/5/57/Something_Just_Like_This.png",
     url: "https://drive.google.com/uc?export=view&id=1C1W8AJpTcZldKAMVhIEHrZVh_idd2OvH",
-    type: "EDM"
+    type: ["Pop", "EDM"]
   },
 ];
