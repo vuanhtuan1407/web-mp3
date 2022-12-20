0ee0bcde292f59733ad2ba30e652cda5be3fa2e2
vuanhtuan1407 <vuanhtuan1407@gmail.com>
2022-12-20 09:33:49 +0700

    save

--- src/MusicList/MusicList.js
+++ src/MusicList/MusicList.js
@@ -2,22 +2,6 @@ import React from "react";
 import { list } from "./List";
 import MusicProvider from "./MusicProvider";
 
-
-function MusicList() {
-
-  return (
-    <MusicProvider>
-      {list.map((music) => (
-        <div className="music">
-          <img className="music-img" src={music.image} />
-          <div className="music-title">
-            <div>{music.name}</div>
-            <div>{music.artist}</div>
-          </div>
-        </div>
-      ))}
-    </MusicProvider>
-  );
-}
+function MusicList() {}
 
 export default MusicList;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,2 +1,25 @@
 import React from "react";
+import { list } from "../../MusicList/List.js";
 
+const initState = list[0];
+
+const CHANGE_AUDIO = "change_audio";
+export const changeAudio = (payload) => {
+  return {
+    type: CHANGE_AUDIO,
+    payload: payload,
+  };
+};
+
+function rootReducer(state = initState, action) {
+  switch (action.type) {
+    case CHANGE_AUDIO:
+      let index = action.payload.id;
+      return list[index];
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+
+export default rootReducer;
