dad62676b2a62e70aeb690bf10c3575e59cfe951
vuanhtuan1407 <vuanhtuan1407@gmail.com>
2023-02-20 16:51:20 +0700

    update final

--- src/redux/reducers/listReducer.js
+++ src/redux/reducers/listReducer.js
@@ -1,16 +1,24 @@
 import { list } from "../../MusicList/List.js";
 import { systemAction } from "../action/action.js";
 
-const initState = { audioList: list };
+const initState = JSON.parse(localStorage.getItem("music-list") || "null") || {
+  audioList: list,
+};
 
 function listReducer(state = initState, action) {
   switch (action.type) {
     case systemAction.UPDATE_LIST: {
-      let newList = action.payload.list;
-      return {
+      const newState = {
         ...state,
         ...action.payload,
       };
+      const jsonState = JSON.stringify(newState);
+      localStorage.setItem("music-list", jsonState);
+      // return {
+      //   ...state,
+      //   ...action.payload,
+      // };
+      return newState;
     }
 
     default:
--- src/redux/reducers/playerReducer.js
+++ src/redux/reducers/playerReducer.js
@@ -2,42 +2,72 @@ import React from "react";
 import { list } from "../../MusicList/List.js";
 import { playerAction, systemAction } from "../action/action.js";
 
-const initState = { index: 0 };
+const initState = JSON.parse(localStorage.getItem("index") || "null") || {
+  index: 0,
+};
 
 function playerReducer(state = initState, action) {
   switch (action.type) {
     case playerAction.CHANGE_AUDIO: {
       let index = action.payload.index;
-      return {
+      const newState = {
         ...state,
         index,
       };
+      const jsonState = JSON.stringify(newState);
+      localStorage.setItem("index", jsonState);
+      // return {
+      //   ...state,
+      //   index,
+      // };
+      return newState;
     }
 
     case playerAction.NEXT_AUDIO: {
       let index = state.index + 1;
       if (index >= list.length) index = 0;
-      return {
+      const newState = {
         ...state,
         index,
       };
+      const jsonState = JSON.stringify(newState);
+      localStorage.setItem("index", jsonState);
+      // return {
+      //   ...state,
+      //   index,
+      // };
+      return newState;
     }
 
     case playerAction.PREVIOUS_AUDIO: {
       let index = state.index - 1;
       if (index < 0) index = list.length - 1;
-      return {
+      const newState = {
         ...state,
         index,
       };
+      const jsonState = JSON.stringify(newState);
+      localStorage.setItem("index", jsonState);
+      // return {
+      //   ...state,
+      //   index,
+      // };
+      return newState;
     }
 
     case playerAction.RANDOM_AUDIO: {
       let index = Math.floor(Math.random() * list.length);
-      return {
+      const newState = {
         ...state,
         index,
       };
+      const jsonState = JSON.stringify(newState);
+      localStorage.setItem("index", jsonState);
+      // return {
+      //   ...state,
+      //   index,
+      // };
+      return newState;
     }
 
     case playerAction.REPEAT_AUDIO: {
